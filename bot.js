196a01edad2c330c7289e6919c557038cc2ebd92
ArabGangDev <41093102+ArabGangDev@users.noreply.github.com>
2019-01-05 15:08:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on('message', message => {
       let member = client.guilds.get(message.guild.id).members.get(message.author.id);
       let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
       let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-    return message.reply(`**${inviteCount}: عدد الاشخاص الذي دعوتهم هو**`)
+    return message.reply(`Your Invites Are **: ${inviteCount} :grin:**`)
 
 });
 }});
